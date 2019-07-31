b57f3f196785955dea4d93132ea3c03373284013
Steven Laff <steve.laff@gmail.com>
2019-07-31 20:03:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,14 @@
 def using_push(array, element)
+  array.push(element)
 end
 
 def using_unshift(array, element)
+  array.unshift(element)
 end
 
-def using_pop()
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args()
+end
